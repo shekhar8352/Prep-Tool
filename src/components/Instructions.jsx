b52c8eada3fcd7cde75c509fc8c9f77f9f8fb1a9
sudhanshu8352 <82777068+sudhanshu8352@users.noreply.github.com>
2023-11-09 13:29:54 +0530

    changes

---
+++ src/components/Instructions.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Instructions() {
+  return (
+    <div>
+      <h2>Instructions</h2>
+      <ul>
+        <li>Read the questions carefully before answering.</li>
+        <li>You have a limited time to complete the quiz.</li>
+        <li>Each question has a specific time limit.</li>
+        <li>Click the "Next" button to move to the next question.</li>
+        <li>You can flag questions to review later.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Instructions;
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 import QuizAnalysis from "../components/QuizAnalysis";
 import DirectionPanel from "../components/DirectionPanel";
 import Question from "../components/Question";
+import Instructions from "../components/Instructions";
 
 import jeeQuestions from "./question.json";
 
@@ -19,6 +20,7 @@ function Quiz() {
   const [score, setScore] = useState(0);
   const [quizOver, setQuizOver] = useState(false);
   const [timeLeft, setTimeLeft] = useState(60);
+  const [quizStarted, setQuizStarted] = useState(false);
 
   // Timer function to update timeLeft every second
   useEffect(() => {
@@ -93,73 +95,90 @@ function Quiz() {
     }
   };
 
+  const handleStartQuiz = () => {
+    // Start the quiz when the "Start Quiz" button is clicked
+    setQuizStarted(true);
+  };
+
   return (
     <div className="container">
       <h1 className="mt-3 mb-4">Quiz App</h1>
 
-      {quizOver ? (
-        <QuizAnalysis
-          score={score}
-          questions={jeeQuestions}
-          userResponses={userResponses}
-        />
+      {!quizStarted ? ( // Display instructions and "Start Quiz" button before the quiz starts
+        <div>
+          <Instructions />
+          <button onClick={handleStartQuiz} className="btn btn-primary">
+            Start Quiz
+          </button>
+        </div>
       ) : (
-        <>
-          <div className="row">
-            <div className="col-lg-9 col-md-12">
-              <div className="question-panel"></div>
-              <p className="mb-3">Time Left: {timeLeft} seconds</p>
-              <Question
-                question={jeeQuestions[currentQuestion].question}
-                options={jeeQuestions[currentQuestion].options}
-                selectedOption={userResponses[currentQuestion].response}
-                onOptionSelect={handleOptionSelect}
-              />
-              <div className="mt-3">
-                <button
-                  onClick={handlePreviousQuestion}
-                  disabled={currentQuestion === 0}
-                  className="btn btn-primary mr-2"
-                >
-                  Previous
-                </button>
-                <button
-                  onClick={handleFlagQuestion}
-                  className={`btn ${
-                    userResponses[currentQuestion].flagged
-                      ? "btn-warning"
-                      : "btn-info"
-                  } mr-2`}
-                >
-                  {userResponses[currentQuestion].flagged ? "Unflag" : "Flag"}
-                </button>
-                {currentQuestion === jeeQuestions.length - 1 ? (
+
+      <div>
+        {quizOver ? (
+          <QuizAnalysis
+            score={score}
+            questions={jeeQuestions}
+            userResponses={userResponses}
+          />
+        ) : (
+          <>
+            <div className="row">
+              <div className="col-lg-9 col-md-12">
+                <div className="question-panel"></div>
+                <p className="mb-3">Time Left: {timeLeft} seconds</p>
+                <Question
+                  question={jeeQuestions[currentQuestion].question}
+                  options={jeeQuestions[currentQuestion].options}
+                  selectedOption={userResponses[currentQuestion].response}
+                  onOptionSelect={handleOptionSelect}
+                />
+                <div className="mt-3">
                   <button
-                    onClick={handleQuizFinish}
-                    className="btn btn-primary"
+                    onClick={handlePreviousQuestion}
+                    disabled={currentQuestion === 0}
+                    className="btn btn-primary mr-2"
                   >
-                    Submit
+                    Previous
                   </button>
-                ) : (
                   <button
-                    onClick={handleNextQuestion}
-                    className="btn btn-primary"
+                    onClick={handleFlagQuestion}
+                    className={`btn ${
+                      userResponses[currentQuestion].flagged
+                        ? "btn-warning"
+                        : "btn-info"
+                    } mr-2`}
                   >
-                    Next
+                    {userResponses[currentQuestion].flagged ? "Unflag" : "Flag"}
                   </button>
-                )}
+                  {currentQuestion === jeeQuestions.length - 1 ? (
+                    <button
+                      onClick={handleQuizFinish}
+                      className="btn btn-primary"
+                    >
+                      Submit
+                    </button>
+                  ) : (
+                    <button
+                      onClick={handleNextQuestion}
+                      className="btn btn-primary"
+                    >
+                      Next
+                    </button>
+                  )}
+                </div>
+              </div>
+              <div className="col-lg-3 d-lg-block d-md-none">
+                <DirectionPanel
+                  questions={jeeQuestions}
+                  userResponses={userResponses}
+                  onDirectQuestion={handleDirectQuestion}
+                  currentQuestion={currentQuestion}
+                />
               </div>
             </div>
-            <div className="col-lg-3 d-lg-block d-md-none">
-              <DirectionPanel
-                questions={jeeQuestions}
-                userResponses={userResponses}
-                onDirectQuestion={handleDirectQuestion}
-                currentQuestion={currentQuestion}
-              />
-            </div>
-          </div>
-        </>
+          </>
+        )}
+      </div>
       )}
     </div>
     // </div>
